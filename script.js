8eb7541400b5838af99dd306af9b94cb83549fc6
Rikey <rikeyk71@gmail.com>
2023-05-18 18:54:21 +0800

    asfas

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // fetch .csv file's data
+    // Fetch .csv file's data
     fetch('table_input.csv')
-      .then(response => response.text())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch CSV file');
+        }
+        return response.text();
+      })
       .then(data => {
-        // change text into array of row
+        // Change text into an array of rows
         const rows = data.split('\n');
-        
+  
         const table1 = document.getElementById('table1');
         const table2 = document.getElementById('table2');
   
         rows.forEach((row, index) => {
           const [indexValue, value] = row.split(',');
+  
+          // Skip empty rows
+          if (indexValue === '' || value === '') {
+            return;
+          }
+  
           const table1Row = table1.insertRow();
           const cell1_1 = table1Row.insertCell();
           const cell1_2 = table1Row.insertCell();
           cell1_1.innerText = indexValue;
           cell1_2.innerText = value;
-
-          // add class to first row
+  
+          // Add class to first row
           if (index === 0) {
             table1Row.classList.add('bold-row');
             cell1_2.classList.add('text-left');
           }
-
+  
           cell1_2.classList.add('index-column');
-
         });
   
         const getValue = (index) => {
-          return parseInt(table1.rows[index].cells[1].innerText);
+          const row = table1.rows[index];
+          if (row && row.cells.length > 1) {
+            return parseInt(row.cells[1].innerText) || 0;
+          }
+          return 0;
         };
   
         const alphaValue = getValue(5) + getValue(20);
